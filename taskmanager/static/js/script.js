e63090a182971d3f82cec44584a023c1077048df
Henrique Pesciotti <hpesciotti@gmail.com>
2024-08-20 15:57:03 +0000

    Add content from taskmanger lesson 9/16

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
-});
+    // Sidenav Initialization Materialize
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+
+    // Date Picker Initialization Materialize
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"} // Internacionalização (software internationalization)
+        
+    });
+
+    // Dropdown Select Initialization Materialize
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects)
+  });
